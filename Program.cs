00386ff4e455109f00a6a89b80f4b9b6aff7505f
İbrahim Enes ÖZBAY <125896771+Enes-cs@users.noreply.github.com>
2023-03-03 18:16:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace MyDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> anahtarDeger = new MyDictionary<int, string>();
+            anahtarDeger.Add(06, "Ankara");
+            anahtarDeger.Add(07, "Antalya");
+            anahtarDeger.Add(43, "Kütahya");
+        }
+    }
+}
